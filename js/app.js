f8bd9d96f15c82cb5e568288669a6c8d9a7ea32c
Aarnoud Meijer <aarnoudmeijer@gmail.com>
2019-05-27 15:12:38 +0200

    code cleanup

--- js/app.js
+++ js/app.js
@@ -12,15 +12,11 @@ const collection = []; // array for items
 // add items to list
 additem.addEventListener("submit", function (event) {
 	event.preventDefault();
-//	console.log(additem.value);
-	let childs = customer.children;
-//	console.log(childs[0].value);
+
+	// grab input value
 	let iteminput = document.getElementById("iteminput");
-	console.log(iteminput.value);
-	for (var i=0; i < childs.length; i++) {
-		console.log(childs[i]);
-	}
 	collection.push(iteminput.value);
+
 	customer.innerHTML +="<li>"+iteminput.value+"</li>";
 	iteminput.value="";
 	console.log(collection);
@@ -30,7 +26,7 @@ additem.addEventListener("submit", function (event) {
 // add items to basket
 arrow.addEventListener("click", function (event) {
 	var items = customer.children;
-//	console.log(childs);
+
 	for (var i=0; i < items.length; i++) {
 		console.log(items[i]);
 	}
