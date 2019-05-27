ff151ecd8e7743b417cd9e83c45521c944be9081
Aarnoud Meijer <aarnoudmeijer@gmail.com>
2019-05-27 09:45:40 +0200

    bootstrapping

---
+++ js/init.js
@@ -0,0 +1,10 @@
+'use strict';
+// bootstrap zoo app
+
+// config
+
+// init
+const customer = document.getElementById('customer');
+const arrow = document.getElementById('arrow');
+const plan = document.getElementById('plan');
+
