7c9059dc9996f3fdb752155dbe7d5fe814260f78
Aarnoud Meijer <aarnoudmeijer@gmail.com>
2019-05-27 10:43:13 +0200

    added forms

--- js/init.js
+++ js/init.js
@@ -8,3 +8,7 @@ const customer = document.getElementById('customer');
 const arrow = document.getElementById('arrow');
 const plan = document.getElementById('plan');
 
+
+const additem = document.getElementById('additem');
+
+// .sort
